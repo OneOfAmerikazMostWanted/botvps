b36018d9fd6c791987743eff5db06bdc1f1f35cc
jamess <thiagozevedo3@gmail.com>
2021-08-23 22:12:42 -0300

    config files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 const { MessageEmbed } = require('discord.js')
+const 
 
 // const moment = require("moment")
 
